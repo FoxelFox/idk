ca87bae691d21421fd11096021b62cae81c0bb6a
Andre Furchner <storkner@gmail.com>
2018-01-23 21:02:29 +0100

    bootstrap module

--- client/main.ts
+++ client/main.ts
@@ -1,1 +1,4 @@
-import "./app/app.module";
+import {AppModule} from "./app/app.module";
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
+
+platformBrowserDynamic().bootstrapModule(AppModule);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,23 +3,37 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-    entry: './client/main.ts',
+    entry: {
+        app: './client/main.ts',
+        vendor: './client/vendor.ts',
+        polyfills: './client/polyfills.ts'
+
+    },
     output: {
         path: path.resolve(__dirname, '../bin/client'),
-        filename: 'bundle.js'
+        filename: '[name].js'
     },
     resolve: {
         extensions: ['.ts', '.js']
     },
     module: {
         rules: [
-            { test: /\.ts$/, loader: 'awesome-typescript-loader', options: {
-                    configFileName: "client/tsconfig.json"
-                }
-            }
+            {
+                test: /\.ts$/,
+                loaders: [{
+                    loader: 'awesome-typescript-loader',
+                    options: { configFileName: "client/tsconfig.json" }
+                }, 'angular2-template-loader']
+            }, {
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
         ]
     },
     plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ['app', 'vendor', 'polyfills']
+        }),
         new webpack.optimize.UglifyJsPlugin(),
         new HtmlWebpackPlugin({template: './client/main.html',filename: 'index.html'})
     ]
